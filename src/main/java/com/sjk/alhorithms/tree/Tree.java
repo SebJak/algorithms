485b7c075c66ddf9433c34b3ed90149aeb57f255
Sebastian <sjak90@gmail.com>
2017-04-26 22:49:45 +0200

    Add StackProfit and simple implementation of Tree

---
+++ src/main/java/com/sjk/alhorithms/tree/Tree.java
@@ -0,0 +1,14 @@
+package com.sjk.alhorithms.tree;
+
+public class Tree {
+    int value;
+    Tree left;
+    Tree right;
+
+    public Tree(int value, Tree left, Tree right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+}
